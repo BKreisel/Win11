b1d51965cb709be0d1c95360b03e61e7368d7589
Brandon Kreisel <BKreisel@users.noreply.github.com>
2022-02-03 10:54:58 -0700

    Update justdoit.ps1

--- justdoit.ps1
+++ justdoit.ps1
@@ -1,5 +1,5 @@
 <# Classic Context Menu
        Don't show the simplified (and much less useful) right-click menu.
 #>
-Set-ItemProperty -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Name '(Default)' -Value 'test'
+Set-ItemProperty -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Name '(Default)' -Value ''
 
