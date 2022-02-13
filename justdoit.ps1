cc32465c59d719f36b8ddcec8f73b17573a4f152
Brandon Kreisel <BKreisel@users.noreply.github.com>
2022-02-13 11:29:29 -0700

    Left Align Taskbar.

--- justdoit.ps1
+++ justdoit.ps1
@@ -3,3 +3,7 @@
 #>
 Set-ItemProperty -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Name '(Default)' -Value ''
 
+<# Left Align the Task Bar. 
+       It looks objectively worse in the center. 
+#>
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name 'TaskbarAl' -Value 0 -Type DWORD
